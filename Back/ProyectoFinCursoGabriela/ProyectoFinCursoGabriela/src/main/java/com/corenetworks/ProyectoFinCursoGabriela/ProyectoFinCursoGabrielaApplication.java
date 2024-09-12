d3f5ab012b3d30d4d786531c0f6e48329112689c
manana <francisco_dominguez_leiva@hotmail.com>
2024-09-12 12:00:21 +0200

    Iniciando el repositorio

---
+++ Back/ProyectoFinCursoGabriela/ProyectoFinCursoGabriela/src/main/java/com/corenetworks/ProyectoFinCursoGabriela/ProyectoFinCursoGabrielaApplication.java
@@ -0,0 +1,13 @@
+package com.corenetworks.ProyectoFinCursoGabriela;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoFinCursoGabrielaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoFinCursoGabrielaApplication.class, args);
+	}
+
+}
